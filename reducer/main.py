2463c5664767328330f0d14e825f58bc584a8fae
Pedro <bubuli.pedro@gmail.com>
2025-09-21 17:13:17 -0300

    filter consume de una cola y produce para una segunda queue, reducer consume de queue2

--- reducer/main.py
+++ reducer/main.py
@@ -5,14 +5,12 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmq'))
 
 channel = connection.channel()
-channel.queue_declare(queue='filterq1')
 channel.queue_declare(queue='reducerq1', durable=True)
 
+
 def callback(ch, method, properties, body):
-    print("llego: %r" % body)
-    body = body.decode('utf-8').upper()
-    channel.basic_publish(exchange='', routing_key='reducerq1', body=body)
+    print("reduje esto: %r" % body)
 
 channel.basic_consume(
-    queue='filterq1', on_message_callback=callback, auto_ack=True)
+    queue='reducerq1', on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
