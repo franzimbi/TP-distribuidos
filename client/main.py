5039f2bee7cd728cda8c4962cae91695e530cf4f
Pedro <bubuli.pedro@gmail.com>
2025-09-21 18:05:42 -0300

    ahora se pasan un csv a traves de rabbit

--- client/main.py
+++ client/main.py
@@ -8,8 +8,13 @@ channel = connection.channel()
 
 channel.queue_declare(queue='filterq1')
 
-for i in range(5):
-    channel.basic_publish(exchange='', routing_key='filterq1', body='hola Peter Buu{}'.format(i))
-    time.sleep(5)
+with open('stores.csv', 'r') as stores:
+    for line in stores:
+        channel.basic_publish(exchange='', routing_key='filterq1', body=line.strip())
+        time.sleep(3)
+
+# for i in range(5):
+#     channel.basic_publish(exchange='', routing_key='filterq1', body='hola Peter Buu{}'.format(i))
+#     time.sleep(5)
 
 connection.close()
