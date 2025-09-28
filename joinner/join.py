a48bcdd7475fa7a03ab9a4194828d9ca48433b41
franzimbi <fzimbimbakis@fi.uba.ar>
2025-09-28 17:31:53 -0300

    join q3 conectado y recibiendo stores. no cierra bien

--- joinner/join.py
+++ joinner/join.py
@@ -19,12 +19,11 @@ class Join:
     def callback_to_receive_join_data(self, ch, method, properties, message):
         batch = Batch()
         batch.decode(message)
-        print(batch)
-        # id = batch.index_of(self.column_id)
-        # name = batch.index_of(self.column_name)
-        # for row in batch:
-        #     if row[id] not in self.join_dictionary:
-        #         self.join_dictionary[row[id]] = row[name]
+        id = batch.index_of(self.column_id)
+        name = batch.index_of(self.column_name)
+        for row in batch:
+            if row[id] not in self.join_dictionary:
+                self.join_dictionary[row[id]] = row[name]
 
         if batch.is_last_batch():
             self.join_queue.stop_consuming()
