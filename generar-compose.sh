e0ddd4d507778336e2cd973411d80e1f9f526215
franzimbi <fzimbimbakis@fi.uba.ar>
2025-10-05 18:48:52 -0300

    listo generador

--- generar-compose.sh
+++ generar-compose.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-echo "Cantidad de nodos: $2"
-python3 mi-generador.py $2
+
+if [ $# -lt 2 ]; then
+  echo "Uso: $0 <cantidad_nodos> <cantidad_clientes>"
+  exit 1
+fi
+
+echo "Cantidad de nodos: $1"
+echo "Cantidad de clientes: $2"
+
+# Verificar si PyYAML está instalado
+if ! python3 -c "import yaml" &> /dev/null; then
+  echo "[INFO] PyYAML no está instalado. Instalando..."
+  pip3 install pyyaml --quiet
+  if [ $? -ne 0 ]; then
+    echo "[ERROR] No se pudo instalar PyYAML. Verifica tu entorno o conexión a Internet."
+    exit 1
+  fi
+  echo "[INFO] PyYAML instalado correctamente."
+fi
+
+# Ejecutar el generador
+python3 generator.py "$1" "$2"
--- generator.py
+++ generator.py
@@ -2,10 +2,14 @@
 import sys
 import yaml
 
+if len(sys.argv) != 3:
+    print("Uso: python3 generador.py <cantidad nodos> <cantidad clientes>")
+    sys.exit(1)
 
-# if len(sys.argv) != 2:
-#     print("Uso: python3 generador.py N")
-#     sys.exit(1)
+cant_nodos = int(sys.argv[1])
+cant_clientes = int(sys.argv[2])
+nombre_file = 'docker-compose-dev.yaml'
+
 
 def crear_distributor():
     data = {
@@ -16,11 +20,11 @@ def crear_distributor():
         'restart': 'on-failure',
         'environment': [
             'PYTHONUNBUFFERED = 1',
-            'Q1result=Q1result',
-            'Q21result=Q21result',
-            'Q22result=Q22result',
-            'Q3result=Q3result',
-            'Q4result=Q4result',
+            'Q1result=Queue_final_Q1',
+            'Q21result=Queue_final_Q21',
+            'Q22result=Queue_final_Q22',
+            'Q3result=Queue_final_Q3',
+            'Q4result=Queue_final_Q4',
             'transactionsExchange=transactionsExchange',
             'itemsExchange=itemsExchange',
             'productsExchange=productsExchange',
@@ -41,9 +45,12 @@ def crear_filters(nombre, cantidad, entrada, salida, type):
     filters = {}
     queues_to_coordinator = ''
     for i in range(1, cantidad + 1):
-        filter_name = f'filter{nombre}_{i}'
+        filter_name = f'{nombre}_{i}'
         conection_coordinator = f'{filter_name}_receive_from_coordinator_{nombre}'
-        queues_to_coordinator += f',{conection_coordinator}'
+        if i == 1:
+            queues_to_coordinator += f'{conection_coordinator}'
+        else:
+            queues_to_coordinator += f',{conection_coordinator}'
         filters[filter_name] = {
             'build': {
                 'context': '.',
@@ -57,8 +64,8 @@ def crear_filters(nombre, cantidad, entrada, salida, type):
                 'PYTHONUNBUFFERED=1',
                 'tipoEntrada=' + entrada,
                 'queueEntrada=entradaFilter' + nombre + '_' + str(i),
-                'tipoSalida=queue',
-                # 'queuesSalida=' + salida,
+                # 'tipoSalida=queue',
+                'queuesSalida=' + salida,
                 'queue_to_send_coordinator=coodinator_' + str(nombre) + '_' + 'unique_queue',
                 'queue_to_receive_coordinator=' + conection_coordinator,
                 'filter_name=' + type
@@ -74,12 +81,12 @@ def crear_filters(nombre, cantidad, entrada, salida, type):
     filters[coordinator_name] = {
         'build': {
             'context': '.',
-            'dockerfile': 'filter/Dockerfile',
+            'dockerfile': 'coordinator/Dockerfile',
         },
         'restart': 'on-failure',
         'environment': [
             'PYTHONUNBUFFERED=1',
-            'consume_queue=coodinator_' + str(nombre) + '_' + 'unique_queue',
+            'queue_entrada=coodinator_' + str(nombre) + '_' + 'unique_queue',
             'num_nodes=' + str(cantidad),
             'queues_to_send_to_nodes=' + queues_to_coordinator,
             # 'tipoSalida=queue',
@@ -96,13 +103,255 @@ def crear_filters(nombre, cantidad, entrada, salida, type):
     return filters
 
 
-with open('text.yaml', 'w') as f:
+def crear_aggregators(nombre, cantidad, entrada, salida, type, params):
+    aggregators = {}
+    # queues_to_coordinator = ''
+    for i in range(1, cantidad + 1):
+        aggregator_name = f'aggregator{nombre}_{i}'
+        aggregators[aggregator_name] = {
+            'build': {
+                'context': '.',
+                'dockerfile': 'aggregator/Dockerfile',
+            },
+            'depends_on': [
+                'distributor'
+            ],
+            'restart': 'on-failure',
+            'environment': [
+                'PYTHONUNBUFFERED=1',
+                'queueEntrada=' + entrada,
+                'queuesSalida=' + salida,
+                'type=' + type,
+                'params=' + params,
+            ],
+            'networks': [
+                'mynet'
+            ],
+            'volumes': [
+                './config.ini:/app/config.ini'
+            ],
+        }
+    return aggregators
+
+
+def crear_reducers(nombre, cantidad, entrada, salida, top, params):
+    reducers = {}
+    # queues_to_coordinator = ''
+    for i in range(1, cantidad + 1):
+        reducer_name = f'reducer{nombre}_{i}'
+        reducers[reducer_name] = {
+            'build': {
+                'context': '.',
+                'dockerfile': 'reducer/Dockerfile',
+            },
+            'depends_on': [
+                'distributor'
+            ],
+            'restart': 'on-failure',
+            'environment': [
+                'PYTHONUNBUFFERED=1',
+                'queueEntrada=' + entrada,
+                'queuesSalida=' + salida,
+                'top=' + str(top),
+                'params=' + params
+            ],
+            'networks': [
+                'mynet'
+            ],
+            'volumes': [
+                './config.ini:/app/config.ini'
+            ],
+        }
+    return reducers
+
+
+def crear_joiners(nombre, cantidad, entradaJoin, entradaData, salida, disk_type, params):
+    joiners = {}
+    queues_to_coordinator = ''
+    for i in range(1, cantidad + 1):
+        joiner_name = f'{nombre}_{i}'
+        conection_coordinator = f'{joiner_name}_receive_from_coordinator_{nombre}'
+        if i == 1:
+            queues_to_coordinator += f'{conection_coordinator}'
+        else:
+            queues_to_coordinator += f',{conection_coordinator}'
+        joiners[joiner_name] = {
+            'build': {
+                'context': '.',
+                'dockerfile': 'joinner/Dockerfile',
+            },
+            'depends_on': [
+                'distributor'
+            ],
+            'restart': 'on-failure',
+            'environment': [
+                'PYTHONUNBUFFERED=1',
+                'entradaJoin=' + entradaJoin,
+                'queueEntradaJoin=entradaJoiner' + nombre + '_' + str(i),
+                'queueEntradaData=' + entradaData,
+                # 'tipoSalida=queue',
+                'queuesSalida=' + salida,
+                'queue_to_send_coordinator=coodinator_' + str(nombre) + '_' + 'unique_queue',
+                'queue_to_receive_coordinator=' + conection_coordinator,
+                'use_diskcache=' + str(disk_type),
+                'params=' + params
+            ],
+            'networks': [
+                'mynet'
+            ],
+            'volumes': [
+                './config.ini:/app/config.ini'
+            ],
+        }
+
+    coordinator_name = f'coordinator_{nombre}'
+    joiners[coordinator_name] = {
+        'build': {
+            'context': '.',
+            'dockerfile': 'coordinator/Dockerfile',
+        },
+        'restart': 'on-failure',
+        'environment': [
+            'PYTHONUNBUFFERED=1',
+            'queue_entrada=coodinator_' + str(nombre) + '_' + 'unique_queue',
+            'num_nodes=' + str(cantidad),
+            'queues_to_send_to_nodes=' + queues_to_coordinator,
+            # 'tipoSalida=queue',
+            'queue_to_send_last_batch=' + salida,
+        ],
+        'networks': [
+            'mynet'
+        ],
+        'volumes': [
+            './config.ini:/app/config.ini'
+        ],
+    }
+    return joiners
+
+
+def crear_client(cantidad, puerto):
+    clients = {}
+    for i in range(1, cantidad + 1):
+        client_name = f'cliente_{i}'
+        clients[client_name] = {
+            'build': {
+                'context': '.',
+                'dockerfile': 'client/Dockerfile',
+            },
+            'restart': 'on-failure',
+            'depends_on': [
+                'distributor'
+            ],
+            'links': [
+                'distributor'
+            ],
+            'environment': [
+                'PYTHONUNBUFFERED=1',
+                'DISTRIBUTOR_HOST=distributor',
+                'DISTRIBUTOR_PORT=' + str(puerto),
+            ],
+            'volumes': [
+                './config.ini:/app/config.ini',
+                './csvs_files:/app/csvs_files',
+                './csvs_files_reduced:/app/csvs_files_reduced',
+                './results' + '_' + client_name + ':/app/results'
+            ],
+            'networks': [
+                'mynet'
+            ],
+        }
+    return clients
+
+
+
+with open(nombre_file, 'w') as f:
     services = {'distributor': crear_distributor()}
-    services.update(crear_filters(nombre='Anio1', cantidad=2, entrada='exchange,itemsExchange',
+    services.update(crear_filters(nombre='Anio1', cantidad=cant_nodos, entrada='exchange,itemsExchange',
                                   salida='Queue_begin2_1,Queue_begin2_2', type='byyear'))
-    services.update(crear_filters(nombre='Anio2', cantidad=2, entrada='exchange,transactionExchange',
+    services.update(crear_filters(nombre='Anio2', cantidad=cant_nodos, entrada='exchange,transactionExchange',
                                   salida='Queue_begin_4,Queue_begin_3_y_1', type='byyear'))
-    services.update(crear_filters(nombre='Hora1', cantidad=2, entrada='queue,Queue_begin_3_y_1',
-                                  salida='Queue_3, Queue_1', type='bytime'))
-    data = {'services': services}
-    yaml.dump(data, f, sort_keys=False, default_flow_style=False)
+    services.update(crear_filters(nombre='Hora1', cantidad=cant_nodos, entrada='queue,Queue_begin_3_y_1',
+                                  salida='Queue_3,Queue_1', type='bytime'))
+    services.update(crear_aggregators(nombre='Suma_Q21', cantidad=cant_nodos, entrada='Queue_begin2_1',
+                                      salida='Queue_between_aggregator_reducer_Q21', type='sum',
+                                      params='item_id,quantity,month,year_month,created_at,total_quantity'))
+    #             equivalente al viejo:
+    #                               - AGGREGATOR_NAME=sum               type
+    #                               - KEY_COLUMN=item_id                params[0]
+    #                               - VALUE_COLUMN=quantity             params[1]
+    #                               - BUCKET_KIND=month                 params[2]
+    #                               - BUCKET_NAME=year_month            params[3]
+    #                               - TIME_COL=created_at               params[4]
+    #                               - OUT_VALUE=total_quantity          params[5]
+
+    services.update(crear_aggregators(nombre='Suma_Q22', cantidad=1, entrada='Queue_begin2_2',
+                                      salida='Queue_between_aggregator_reducer_Q22', type='sum',
+                                      params='earnings,item_id,subtotal,month,year_month,created_at,total_earnings'))
+    services.update(crear_aggregators(nombre='Suma_Q3', cantidad=1, entrada='Queue_3',
+                                      salida='Queue_between_aggregator_join_Q3', type='sum',
+                                      params='store_id,final_amount,semester,year_semester,created_at,tpv'))
+    #               equivalente al viejo:
+    #                       - AGGREGATOR_NAME=sum               type
+    #                       - KEY_COLUMN=store_id               params[0]
+    #                       - VALUE_COLUMN=final_amount         params[1]
+    #                       - BUCKET_KIND=semester              params[2]
+    #                       - BUCKET_NAME=year_semester         params[3]
+    #                       - TIME_COL=created_at               params[4]
+    #                       - OUT_VALUE=tpv                     params[5]
+    services.update(crear_aggregators(nombre='Counter_Q4', cantidad=1, entrada='Queue_begin_4',
+                                      salida='Queue_between_aggregator_reducer_Q4', type='counter',
+                                      params='store_id,user_id,purchases_qty'))
+    #               equivalente al viejo:
+    #                       - KEY_COLUMNS=store_id,user_id
+    #                       - COUNT_NAME=purchases_qty
+    #                       - AGGREGATOR_NAME=counter
+    services.update(crear_reducers(nombre='Reducer_Q21', cantidad=1, entrada='Queue_between_aggregator_reducer_Q21',
+                                   salida='Queue_between_reducer_joiner_Q21', top=1,
+                                   params='year_month,item_id,total_quantity'))
+    services.update(crear_reducers(nombre='Reducer_Q22', cantidad=1, entrada='Queue_between_aggregator_reducer_Q22',
+                                   salida='Queue_between_reducer_joiner_Q22', top=1,
+                                   params='year_month,item_id,total_earnings'))
+
+    services.update(crear_reducers(nombre='Reducer_Q4', cantidad=1, entrada='Queue_between_aggregator_reducer_Q4',
+                                   salida='Queue_between_reducer_joiner_Q4', top=3,
+                                   params='store_id,user_id,purchases_qty'))
+    services.update(crear_filters(nombre='Filter_amount_Q1', cantidad=cant_nodos, entrada='queue,Queue_1',
+                                  salida='Queue_between_filter_amount_filter_columna_Q1', type='byamount'))
+    services.update(crear_filters(nombre='Filter_column_Q1', cantidad=cant_nodos,
+                                  entrada='queue,Queue_between_filter_amount_filter_columna_Q1',
+                                  salida='Queue_final_Q1', type='bycolumn'))
+
+    services.update(
+        crear_joiners(nombre='Join_productos_Q21', cantidad=cant_nodos, entradaJoin='exchange,productsExchange',
+                      entradaData='Queue_between_reducer_joiner_Q21', salida='Queue_final_Q21',
+                      disk_type=False, params='item_name,item_id'))
+    #                   equivalente al viejo:
+    #                           - COLUMN_NAME=item_name
+    #                           - COLUMN_ID=item_id
+    services.update(
+        crear_joiners(nombre='Join_productos_Q22', cantidad=cant_nodos, entradaJoin='exchange,productsExchange',
+                      entradaData='Queue_between_reducer_joiner_Q22', salida='Queue_final_Q22',
+                      disk_type=False, params='item_name,item_id'))
+    services.update(crear_joiners(nombre='Join_stores_Q3', cantidad=cant_nodos, entradaJoin='exchange,storesExchange',
+                                  entradaData='Queue_between_aggregator_join_Q3', salida='Queue_final_Q3',
+                                  disk_type=False, params='store_name,store_id'))
+    services.update(crear_joiners(nombre='Join_users_Q4', cantidad=cant_nodos, entradaJoin='exchange,usersExchange',
+                                  entradaData='Queue_between_reducer_joiner_Q4',
+                                  salida='Queue_between_joiner_users_and_joiner_stores_Q4',
+                                  disk_type=True, params='item_name,item_id'))
+    services.update(crear_joiners(nombre='Join_stores_Q4', cantidad=cant_nodos, entradaJoin='exchange,storesExchange',
+                                  entradaData='Queue_between_joiner_users_and_joiner_stores_Q4',
+                                  salida='Queue_final_Q4',
+                                  disk_type=False, params='store_name,store_id'))
+
+    services.update(crear_client(cant_clientes, 5000))
+    data = {'services': services,
+            'networks': {
+                'mynet': {
+                    'external': False,
+                    'driver': 'bridge',
+                }
+            }
+            }
+
+    yaml.dump(data, f, sort_keys=False, default_flow_style=False, line_break='\n', explicit_start=True)
